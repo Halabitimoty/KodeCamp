7f167ac9ebef76f65a3d45b73632967c78886d18
Halabitimoty <57334680+Halabitimoty@users.noreply.github.com>
2023-09-10 14:31:52 +0100

    Staged 1

--- 0x03_frontend-scaffold/functionalities.js
+++ 0x03_frontend-scaffold/functionalities.js
@@ -1,11 +1,18 @@
 const fs = require("fs");
 
 function create_folder(path) {
-  fs.mkdir(path, (error) => {
-    if (error) {
-      console.error(`Can't create Folder.\n${error}`);
+  fs.access(path, (err) => {
+    if (err) {
+      console.error("Folder does not exist:");
+      fs.mkdir(path, (error) => {
+        if (error) {
+          console.error(`Can't create Folder.\n${error}`);
+        } else {
+          console.log(`Folder ${path} created successfully.`);
+        }
+      });
     } else {
-      console.log(`Folder ${path} created successfully.`);
+      console.log("Folder exists.");
     }
   });
 }
@@ -13,7 +20,7 @@ function create_folder(path) {
 function create_file(fileName, fileContent) {
   fs.writeFile(fileName, fileContent, (err) => {
     if (err) {
-      console.error("Error creating the file:", err);
+      console.error("Error creating the file:");
     } else {
       console.log(`File '${fileName}' has been created successfully.`);
     }
