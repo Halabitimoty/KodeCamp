65c66e9975ac1f5cd7d2b1d5d170ed97b63ae2cc
Halabitimoty <57334680+Halabitimoty@users.noreply.github.com>
2023-09-11 10:36:58 +0100

    Task: Update

--- 0x00_NOTE/ChildProcess.js
+++ 0x00_NOTE/ChildProcess.js
@@ -1,12 +1,43 @@
 const cp = require("child_process");
+const { on } = require("events");
+const { exit } = require("process");
 
 const progs = {
   list: "ls",
   copy: "cp",
   folder: "mkdir",
+  file: "touch",
 };
 
 let child = cp.spawn(progs.list);
+let copy = cp.spawn(progs.copy, ["spawn.txt", "spawn.txt"]);
+let file = cp.spawn(progs.file, ["spawn.txt"]);
+
+/**
+ * ----Process----
+ */
+
 child.stdout.on("data", (stream) => {
   console.log(`${stream}`);
+});
+
+child.stderr.on("data", (err) => {
+  console.error(err);
+});
+
+//Create File
+file.stdout.on("close", () => {
+  console.log("File Created Successfully");
+});
+
+file.stderr.on("close", (err) => {
+  console.log("Unable to Create file.");
+});
+
+//Copy File
+copy.stderr.on("data", (err) => {
+  console.log("There was an error Copying File.");
+});
+copy.stdout.on("exit", () => {
+  console.log("copy process finished");
 });
